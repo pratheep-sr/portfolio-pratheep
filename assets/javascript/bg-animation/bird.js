3a225fd03fa5282902448c11e152072cc55d2895
pratheep-sr <pratheepjenish75@gmail.com>
2026-01-07 18:19:50 +0530

    firt upload

---
+++ assets/javascript/bg-animation/bird.js
@@ -0,0 +1,17 @@
+
+/******************** Birds Effect JS ********************/
+VANTA.BIRDS({
+  el: "#birds_bg_effect",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+color1: 0x15CCDA, // Updated to #15ccda
+color2: 0x15CCDA, // Updated to #15ccda (if you want same color)
+
+
+  backgroundColor: 0x000000, // Black
+})
